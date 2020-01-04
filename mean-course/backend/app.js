5703205a85e96c00f9717bd389c1137ec7210f4e
jeevan.mv <jeevan.mv@sonata-software.com>
2020-01-04 20:10:32 +0530

    Working with mongoDB

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -1,7 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+
+// mongoDB Atlas:
+// jeevanmvas
+// pwd - as3cvKseCiujpsma
+// Current IP address - 121.244.199.178 (Sonata GV2 BG4WS0850)
+
+// 'node-angular' is name of DB created on the fly
+mongoose.connect('mongodb+srv://jeevanmvas:as3cvKseCiujpsma@cluster0-uid3b.mongodb.net/node-angular?retryWrites=true&w=majority', {useNewUrlParser: true})
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection failed');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,30 +37,35 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
-  res.status(201).json({
-    message: 'Post added successfully'
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save()
+  .then(createdPost => {
+    res.status(201).json({
+      message: 'Post added successfully',
+      postId: createdPost._id
+    });
   });
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "fadf12421l",
-      title: "First server-side post",
-      content: "This is coming from the server"
-    },
-    {
-      id: "ksajflaj132",
-      title: "Second server-side post",
-      content: "This is coming from the server!"
-    }
-  ];
-  res.status(200).json({
-    message: "Posts fetched successfully!",
-    posts: posts
-  });
+  Post.find()
+  .then(documents => {
+    res.status(200).json({
+      message: "Posts fetched successfully!",
+      posts: documents
+    });
+  })
 });
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id})
+  .then(result => {
+    console.log(result);
+    res.status(200).json({message: 'Post deleted!'});
+  })
+})
 
 module.exports = app;
